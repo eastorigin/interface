4a919cabdc743f315fd58011494e8f8a08618244
Shin Dong Won <eastorigin21@gmail.com>
2024-07-26 16:21:45 +0900

    12일차 수업 내용

--- src/com/ktdsuniversity/edu/interfaceexam/addresses/AddressBook.java
+++ src/com/ktdsuniversity/edu/interfaceexam/addresses/AddressBook.java
@@ -1,97 +1,97 @@
-package com.ktdsuniversity.edu.interfaceexam.addresses;
-
-public class AddressBook implements Address{
-	
-	/*
-	 * 이름
-	 * 주소
-	 * 연락처
-	 * 이메일
-	 * 생일
-	 * 음력/양력
-	 * 성별
-	 */
-	
-	private Person[] persons;
-	private int nowIndex;
-	
-	public AddressBook() {
-		this(100);
-	}
-	
-	public AddressBook(int size) {
-		this.persons = new Person[size];
-	}
-
-	@Override
-	public void creatNewAddress(Person newPerson) {
-		this.persons[nowIndex++] = newPerson;
-		
-	}
-
-	@Override
-	public void readAllAddresses() {
-		
-	}
-
-	@Override
-	public void readOneAddress() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateOneAddress() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteAllAddresses() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteOneAddress() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void creatNewAddress(Person newPerson) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void readAllAddresses(String name) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void readOneAddress(int index) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateOneAddress(int index, Person newInfoPerson) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteOneAddress(int index) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteAddresses(int... indexes) {
-		// TODO Auto-generated method stub
-		
-	}
-
-}
+//package com.ktdsuniversity.edu.interfaceexam.addresses;
+//
+//public class AddressBook implements Address{
+//	
+//	/*
+//	 * 이름
+//	 * 주소
+//	 * 연락처
+//	 * 이메일
+//	 * 생일
+//	 * 음력/양력
+//	 * 성별
+//	 */
+//	
+//	private Person[] persons;
+//	private int nowIndex;
+//	
+//	public AddressBook() {
+//		this(100);
+//	}
+//	
+//	public AddressBook(int size) {
+//		this.persons = new Person[size];
+//	}
+//
+//	@Override
+//	public void creatNewAddress(Person newPerson) {
+//		this.persons[nowIndex++] = newPerson;
+//		
+//	}
+//
+//	@Override
+//	public void readAllAddresses() {
+//		
+//	}
+//
+//	@Override
+//	public void readOneAddress() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void updateOneAddress() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void deleteAllAddresses() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void deleteOneAddress() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void creatNewAddress(Person newPerson) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void readAllAddresses(String name) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void readOneAddress(int index) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void updateOneAddress(int index, Person newInfoPerson) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void deleteOneAddress(int index) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void deleteAddresses(int... indexes) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//}
--- src/com/ktdsuniversity/edu/interfaceexam/addresses/Main.java
+++ src/com/ktdsuniversity/edu/interfaceexam/addresses/Main.java
@@ -1,21 +1,21 @@
-package com.ktdsuniversity.edu.interfaceexam.addresses;
-
-public class Main {
-
-	public static void main(String[] args) {
-		AddressBook addressBook = new AddressBook();
-		
-		Person p1 = new Person("kim", "seoul", "010-1234-1234", "kim@kim.co.kr");
-		addressBook.creatNewAddress(p1);
-		addressBook.creatNewAddress(p1);
-		addressBook.creatNewAddress(p1);
-		addressBook.creatNewAddress(p1);
-		addressBook.creatNewAddress(p1);
-		addressBook.creatNewAddress(p1);
-		addressBook.creatNewAddress(p1);
-		
-		addressBook.readOneAddress(0);
-		
-		// Class.staticMethod.instanceMethod();
-	}
-}
+//package com.ktdsuniversity.edu.interfaceexam.addresses;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		AddressBook addressBook = new AddressBook();
+//		
+//		Person p1 = new Person("kim", "seoul", "010-1234-1234", "kim@kim.co.kr");
+//		addressBook.creatNewAddress(p1);
+//		addressBook.creatNewAddress(p1);
+//		addressBook.creatNewAddress(p1);
+//		addressBook.creatNewAddress(p1);
+//		addressBook.creatNewAddress(p1);
+//		addressBook.creatNewAddress(p1);
+//		addressBook.creatNewAddress(p1);
+//		
+//		addressBook.readOneAddress(0);
+//		
+//		// Class.staticMethod.instanceMethod();
+//	}
+//}
